a8914efc30d33c6d582d8f5502d9979d01158166
PAVAN SAMBHAJI NANGARE-PATIL <62687233+nangarepavan8@users.noreply.github.com>
2021-12-11 00:20:03 +0530

    Linear Queue with some operations

---
+++ LinearQueue/src/LinearQueueMain.java
@@ -0,0 +1,70 @@
+
+class LinearQueue{
+	
+	private int []  arr;
+	private int front , rear;
+	
+	public LinearQueue(int size)
+	{
+		arr = new int [size];
+		front = -1;
+		rear = -1;
+		
+	}
+	
+	public void push(int val) {
+		rear++;
+		arr[rear] = val;
+	}
+	public void pop() {
+		front ++;
+	}
+	
+	public int peek() {
+		
+		return arr[front + 1 ];
+	}
+	public boolean isEmpty() {
+		return front == rear;
+	}
+
+	public boolean isFull() {
+		return rear == arr.length-1;
+	}
+}
+
+
+public class LinearQueueMain {
+
+	public static void main(String[] args) {
+		
+		LinearQueue q = new LinearQueue(6);
+		
+		if(!q.isFull())
+			q.push(11);
+			
+		if(!q.isFull())
+			q.push(22);
+		
+		if(!q.isFull())
+		   q.push(33);
+		
+		if(!q.isFull())
+		q.push(44);
+		
+		if(!q.isFull())
+		q.push(55);
+		
+		if(!q.isFull())
+		q.push(66);
+		
+
+//		System.out.println("Element at front "+ q.peek());
+		while(!q.isEmpty()) {
+			System.out.println("Pooping:"+ q.peek());
+			q.pop();
+		}
+
+	}
+
+}
